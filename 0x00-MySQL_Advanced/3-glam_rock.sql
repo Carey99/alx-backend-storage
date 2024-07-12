8027baa3cc5736fbb8ea86128a50f95834d8bee3
Carey99 <edwinakidah1@gmail.com>
2024-07-12 00:56:54 +0000

    Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Lists all brand with Glam rock ranked by longevity
+SELECT band_name,
+	(IFNULL(split, '2020') - formed) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
