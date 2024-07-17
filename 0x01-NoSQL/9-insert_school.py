8fca695c3d8795da07259845f8d9fd548141d5da
Carey99 <edwinakidah1@gmail.com>
2024-07-17 03:15:50 +0300

    Insert ne doc

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Inserts a new doc in a colection based on kwargs
+"""
+
+
+def insert_school(mongo_colletion, **kwargs):
+    """Inserts a new doc"""
+    added = mongo_collection.insert_one(kwargs)
+    return added.inserted_id
